15211fdba838641f6b2a0296114c2f1c05b245fb
Dariwka <77488950+Dariwka@users.noreply.github.com>
2022-09-13 12:58:13 +0300

    added components of chat box and styles

---
+++ src/pages/Login.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">DiDi chat</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="display name" />
+          <input type="email" placeholder="email" />
+          <button>Sign in</button>
+        </form>
+        <p>You don't have an account? Register</p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
